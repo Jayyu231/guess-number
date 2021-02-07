063e19a8994adbf38c5f1e506fb4abc9eec916dc
Jay Yu <jayyu231@gmail.com>
2021-02-07 22:23:31 +0800

    2nd version

---
+++ guess number_2.py
@@ -0,0 +1,25 @@
+# 產生一個隨機整數1~100 (不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出 "終於猜對了!"
+# 猜錯的話 要告訴他 比答案大/小
+
+#延伸功能 一邊猜一邊告知猜了第幾次?
+
+import random
+r = random.randint(1,100)
+c = 0
+while True :
+	user_input = input('Guess a number')
+	c += 1 #c = c + 1
+	user_input = int(user_input)
+	if user_input == r :
+		print('猜了第', c ,'次')
+		print('終於猜對了!')
+		break
+	elif user_input < r :
+		#print('猜了第', c ,'次')
+		print('答案比較大')
+	elif user_input > r :
+		#print('猜了第', c ,'次') 
+		print('答案比較小')
+	print('這是你猜的第', c ,'次') 
